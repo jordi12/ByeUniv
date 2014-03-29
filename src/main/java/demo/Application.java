ed3b438e3b4de6839096a001c13e2163764aa90f
jordi12 <jordan.blancpoujol@gmail.com>
2014-03-29 22:23:06 +0100

    projet serveur fonctionne

---
+++ src/main/java/demo/Application.java
@@ -0,0 +1,29 @@
+package demo;
+
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.SpringApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+@Configuration
+@EnableAutoConfiguration
+@ComponentScan
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+    /*
+    @Bean
+    public ViewResolver getViewResolver(){
+        InternalResourceViewResolver resolver = new InternalResourceViewResolver();
+        resolver.setPrefix("/src/main/resources/templates");
+        resolver.setSuffix(".html");
+        return resolver;
+    }
+    */
+
+} 
