7ca85ab826d218d9b1feda64059fa407855fd9ef
jordi12 <jordan.blancpoujol@gmail.com>
2014-03-30 20:44:35 +0200

    recuperation url + html

---
+++ src/main/java/demo/AfficherHeureController.java
@@ -0,0 +1,17 @@
+package demo;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class AfficherHeureController {
+	 @RequestMapping("/afficherheure")
+	    public String heure(@RequestParam(value="line", required=true) String line,
+	    		@RequestParam(value="name", required=true) String name,Model model) {
+	        model.addAttribute("name", name);
+	        return "afficherheure";
+	 }
+
+}
--- src/main/java/demo/Application.java
+++ src/main/java/demo/Application.java
@@ -1,5 +1,12 @@
 package demo;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.StringWriter;
+import java.net.URL;
+import java.net.URLConnection;
+
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.context.annotation.Bean;
@@ -15,7 +22,39 @@ public class Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
-    }
+        try{//read from file
+	        StringWriter content = new StringWriter();   
+	        URL url = new URL("http://pt.data.tisseo.fr/departureBoard?operatorCode=5936&format=json&key=a03561f2fd10641d96fb8188d209414d8");  
+	        URLConnection urlConnection = url.openConnection();  
+	        BufferedReader in = new BufferedReader(new InputStreamReader(url.openStream()));  
+	        String line;  
+	          
+	        //read line from bufferedreader and append to to content(stringwriter)   
+	        //while the bufferedreader returns a line  
+	        while( (line = in.readLine()) != null )  
+	        {  
+	          content.write( line );  
+	        }   
+	          
+	        //close down buffered reader and string writer  
+	        in.close();  
+	        content.close();   
+	          
+	        //output the read in text  
+	        String fileText;  
+	        fileText = content.toString();          
+	        System.out.println(fileText);  
+	        //show success message  
+	        System.out.println("Finished reading file!");  
+	          
+      }catch(IOException e){//handle exceptions  
+            
+          System.out.println("Error reading file!");  
+            
+      }//handle exceptions  
+   
+    }//main method  
+}
     /*
     @Bean
     public ViewResolver getViewResolver(){
@@ -26,4 +65,3 @@ public class Application {
     }
     */
 
-} 
